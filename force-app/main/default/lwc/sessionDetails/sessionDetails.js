237e5a6958fec6e1734573803c1dc8bf8ebfc208
taphalow <lmoustapha2@gmail.com>
2022-03-25 12:48:00 +0000

    Retrieve Data Using Apex

--- force-app/main/default/lwc/sessionDetails/sessionDetails.js
+++ force-app/main/default/lwc/sessionDetails/sessionDetails.js
@@ -1,35 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getSession from '@salesforce/apex/SessionController.getSession';
 export default class SessionDetails extends LightningElement {
+  @api sessionId;
   session;
-  @api
-  set sessionId(id) {
-    this._sessionId = id;
-    this.session = {
-      id: '1',
-      name: 'Mock session',
-      dateTime: '2099-01-01 00:00:00',
-      room: 'Mock room',
-      description: "Mock description",
-      speakers: [
-        {
-          id: '1',
-          name: 'Mock speaker 1',
-          bio: 'Bio for mock speaker 1',
-          email: 'mock1@trailhead.com',
-          pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/john_doe.jpg'
-        },
-        {
-          id: '2',
-          name: 'Mock speaker 2',
-          bio: 'Bio for mock speaker 2',
-          email: 'mock2@trailhead.com',
-          pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/laetitia_arevik.jpg'
-        }
-      ]
-    };
-  }
-  get sessionId() {
-    return this._sessionId;
+  @wire(getSession, { sessionId: '$sessionId' })
+  wiredSession({ error, data }) {
+    if (data) {
+      this.session = data;
+    } else if (error) {
+      this.session = undefined;
+      throw new Error('Failed to retrieve session');
+    }
   }
   handleSessionsClick() {
     const navigateEvent = new CustomEvent('navigate', {
@@ -38,5 +19,5 @@ export default class SessionDetails extends LightningElement {
       }
     });
     this.dispatchEvent(navigateEvent);
-  }
+  }  
 }
